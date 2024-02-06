a1b4af20559eba354858d275a1de729380d48465
Faisal <officialfaisal1112@gmail.com>
2024-02-06 11:28:37 +0600

    make some changes

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,24 +1,12 @@
-'use client';
-
 import SecondaryNavBar from '@/components/secondaryNavBar';
 import SideBar from '@/components/sideBar';
 import accountNavItems from '@/constants/accountNavItems';
 import { getUserInfo } from '@/services/auth.service';
 import { redirect } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   const user = getUserInfo();
-
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
 
   if (user.role !== 'admin') {
     redirect('/login');
